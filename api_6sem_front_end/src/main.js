508128e56686695022faafdf1b95a4c3448104c0
raulnt <rjbnetto@gmail.com>
2025-09-16 22:32:13 -0300

    LMN-26 feat: 'Bar Chart' component of the indicator Calls by Period

--- api_6sem_front_end/src/main.js
+++ api_6sem_front_end/src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import naive from 'naive-ui';
+import { Line } from 'vue-chartjs';
 
 createApp(App)
   .use(router)
   .use(naive)
   .mount('#app');
 
+app.component('LineChart', Line)
 
